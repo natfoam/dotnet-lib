87416d382ead2d209627a4ec52bed7203ce70f18
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-07-15 18:07:38 -0700

    .net 6.0 (#2)
    * .Net 6.0
    
    * Pipeline
    
    * simplify

--- EnumerableFunc/IEnumerableFunc.cs
+++ EnumerableFunc/IEnumerableFunc.cs
@@ -3,30 +3,17 @@ using System.Collections.Generic;
 
 namespace EnumerableFunc
 {
+    /// <summary>
+    /// A function which can be applied to any IEnumerable<...> collection.
+    /// </summary>
+    /// <typeparam name="R">A type of the result of the function.</typeparam>
     public interface IEnumerableFunc<R>
     {
         R Do<T>(IEnumerable<T> v);
     }
 
     public static class EnumerableFuncEx {
-        public static R Invoke<R>(this IEnumerable value, IEnumerableFunc<R> func)
-        {
-            IWrap wrap = CreateWrap((dynamic)value);
-            return wrap.Invoke(func);
-        }
-
-        interface IWrap
-        {
-            R Invoke<R>(IEnumerableFunc<R> r);
-        }
-
-        sealed class Wrap<T> : IWrap
-        {
-            public IEnumerable<T> value;
-            public R Invoke<R>(IEnumerableFunc<R> func) => func.Do(value);
-        }
-
-        static IWrap CreateWrap<T>(IEnumerable<T> value) 
-            => new Wrap<T> { value = value };
+        public static R Invoke<R>(this IEnumerable value, IEnumerableFunc<R> func) 
+            => func.Do((dynamic)value);
     }
 }
